b24d415f809259ebf68c0316b86cbf334de00e2e
James Guthrie <jamesrguthrie@gmail.com>
2017-02-07 14:03:35 -0500

    added folders for css files and js files, and added pseudo code to app.js

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// Pseudo Code:
+
+// Tic Tac Toe with Jquery: 
+
+// use grid of 3 columns with 3 div boxes inside each column 
+
+// set up 3 by 3 grid to an array in jquery.
+
+// prompt user to pick x's or O's. 
+
+// Jquery event listener for each box.
+
+// If box is clicked on put user's input choice (x or o)
+
+// |||||||||||||||||||||||||||||||||||||||||||||||||||||
+
